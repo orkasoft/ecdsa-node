d69af7e3e457e48ffc2cbcd12e92a51c3dbde7f9
emreordukaya <emreordukaya@hotmail.com>
2023-10-18 22:36:45 +0300

    Edit for Public Key Cryptography

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,8 +1,13 @@
 import server from "./server";
+import * as serc from 'ethereum-cryptography/secp256k1';
+import  {toHex} from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+
+    const address = toHex(serc.secp256k1.getPublicKey(privateKey)); 
     setAddress(address);
     if (address) {
       const {
@@ -19,10 +24,12 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input placeholder="Type in a private key:" value={privateKey} onChange={onChange}></input>
       </label>
 
+      <div>Address: {address}</div>
+      
       <div className="balance">Balance: {balance}</div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -6,10 +6,19 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
+// private key:  4337aa776fc53f73923a9e11cb0c8bcbd7a4207dd802a2d49868a8efce15de8c
+// public key:  03757d6c6c31f737aa57604374874c47ebaa8f3c09555e2dd440520127142a8e69
+
+// private key:  0deb8a6fe1ee8c1f936dc3e7f6b03791489c0fabf48c3f0ea9399c016e2ea06e
+// public key:  03896dcbb3420a647475902ea254611785bf84c95beaf8210586bfcca8ca2b016c
+
+// private key:  440feea85bb2d76701ff3cb4f140e4d05489933b538c89f59d324ee87dd1fe0b
+// public key:  0234903bef36f7af8005547314f4f7f777d97df0ffcb3bc053e72b9177ee81ee65
+
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "03757d6c6c31f737aa57604374874c47ebaa8f3c09555e2dd440520127142a8e69": 100,
+  "03896dcbb3420a647475902ea254611785bf84c95beaf8210586bfcca8ca2b016c": 50,
+  "0234903bef36f7af8005547314f4f7f777d97df0ffcb3bc053e72b9177ee81ee65": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,6 +28,9 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
+    // To-do Get a signature from the client-side application
+  // Recover the sender from the signature.
+  
   const { sender, recipient, amount } = req.body;
 
   setInitialBalance(sender);
